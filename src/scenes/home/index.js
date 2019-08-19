e6a6288188fb3a42d369317a553708d9899897de
FreakyCoder <kurayogun@gmail.com>
2019-08-19 15:13:04 +0300

    MarkerImage

--- src/scenes/home/index.js
+++ src/scenes/home/index.js
@@ -3,6 +3,9 @@ import PropTypes from 'prop-types';
 import { Text, View } from 'react-native';
 import MapView from 'react-native-map-clustering';
 import { Marker } from 'react-native-maps';
+import Icon from "react-native-dynamic-vector-icons";
+
+const markerImage = require("../../assets/location-marker.png");
 
 const HomeScreen = (props) => {
 	const {} = props;
@@ -15,14 +18,14 @@ const HomeScreen = (props) => {
 				longitudeDelta: 8.5,
 			}}
 			style={{ width: 400, height: 800 }}>
-			<Marker coordinate={{ latitude: 52.0, longitude: 18.2 }} />
-			<Marker coordinate={{ latitude: 52.4, longitude: 18.7 }} />
-			<Marker coordinate={{ latitude: 52.1, longitude: 18.4 }} />
-			<Marker coordinate={{ latitude: 52.6, longitude: 18.3 }} />
-			<Marker coordinate={{ latitude: 51.6, longitude: 18.0 }} />
-			<Marker coordinate={{ latitude: 53.1, longitude: 18.8 }} />
-			<Marker coordinate={{ latitude: 52.9, longitude: 19.4 }} />
-			<Marker coordinate={{ latitude: 52.2, longitude: 21 }} />
+			<Marker coordinate={{ latitude: 52.0, longitude: 18.2 }} image={markerImage} />
+			<Marker coordinate={{ latitude: 52.4, longitude: 18.7 }} image={markerImage} />
+			<Marker coordinate={{ latitude: 52.1, longitude: 18.4 }} image={markerImage} />
+			<Marker coordinate={{ latitude: 52.6, longitude: 18.3 }} image={markerImage} />
+			<Marker coordinate={{ latitude: 51.6, longitude: 18.0 }} image={markerImage} />
+			<Marker coordinate={{ latitude: 53.1, longitude: 18.8 }} image={markerImage} />
+			<Marker coordinate={{ latitude: 52.9, longitude: 19.4 }} image={markerImage} />
+			<Marker coordinate={{ latitude: 52.2, longitude: 21 }} image={markerImage} />
 		</MapView>
 	);
 };
