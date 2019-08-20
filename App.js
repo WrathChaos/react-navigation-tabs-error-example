034b13981f9251488ae1ff852f7b895844d88279
FreakyCoder <kurayogun@gmail.com>
2019-08-20 11:35:45 +0300

    APIService is gonna rewritten

--- App.js
+++ App.js
@@ -6,23 +6,16 @@ import { createAppContainer } from 'react-navigation';
 import NavigationService from 'react-navigation-helpers';
 import configureStore from './src/services/redux/store';
 import { Navigator } from './src/services/navigation';
+import { Api } from './src/services/api';
 import {
   AndroidBackHandler, ErrorHandler, SystemMessageHandler, NetworkHandler, LocationHandler,
 } from './src/services/handlers';
 console.disableYellowBox = true
 // Persistor & Store
 const { persistor, store } = configureStore;
-/**
-   * Ignore some yellowbox warnings. Some of these are for deprecated functions
-   * that we haven't gotten around to replacing yet.
-   */
-YellowBox.ignoreWarnings([
-  'componentWillMount is deprecated',
-  'componentWillUpdate is deprecated',
-  'componentWillReceiveProps is deprecated',
-]);
 
 const App = () => {
+
   const MainNavigator = createAppContainer(Navigator);
   return (
     <Provider store={store}>
