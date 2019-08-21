0431622c0f2832d62004915d53953244b5cf51ab
FreakyCoder <kurayogun@gmail.com>
2019-08-21 16:22:18 +0300

    New eslintrc is integrated

--- App.js
+++ App.js
@@ -1,21 +1,24 @@
-import React from 'react';
-import { View, StatusBar, YellowBox } from 'react-native';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/es/integration/react';
-import { createAppContainer } from 'react-navigation';
-import NavigationService from 'react-navigation-helpers';
-import configureStore from './src/services/redux/store';
-import { Navigator } from './src/services/navigation';
-import { Api } from './src/services/api';
+import React from "react";
+import { View, StatusBar, YellowBox } from "react-native";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/es/integration/react";
+import { createAppContainer } from "react-navigation";
+import NavigationService from "react-navigation-helpers";
+import configureStore from "./src/services/redux/store";
+import { Navigator } from "./src/services/navigation";
 import {
-  AndroidBackHandler, ErrorHandler, SystemMessageHandler, NetworkHandler, LocationHandler,
-} from './src/services/handlers';
-console.disableYellowBox = true
+  AndroidBackHandler,
+  ErrorHandler,
+  SystemMessageHandler,
+  NetworkHandler,
+  LocationHandler
+} from "./src/services/handlers";
+
+console.disableYellowBox = true;
 // Persistor & Store
 const { persistor, store } = configureStore;
 
 const App = () => {
-
   const MainNavigator = createAppContainer(Navigator);
   return (
     <Provider store={store}>
@@ -25,7 +28,9 @@ const App = () => {
             <View style={{ flex: 1 }}>
               <StatusBar barStyle="dark-content" />
               <MainNavigator
-                ref={(navigatorRef) => NavigationService.setGlobalLevelNavigator(navigatorRef)}
+                ref={navigatorRef =>
+                  NavigationService.setGlobalLevelNavigator(navigatorRef)
+                }
               />
             </View>
             {/* FIXME: Fix these handlers from Redux */}
